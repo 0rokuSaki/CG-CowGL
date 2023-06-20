9324671194d286655c6add8c283aa026e63ad96e
BAAARON2 <BAAARON2@365.openu.ac.il>
2023-06-20 19:01:55 +0300

    Changed data type to GLfloat

--- WcPt3D.h
+++ WcPt3D.h
@@ -20,16 +20,16 @@ class WcPt3D
 {
 public:
     WcPt3D() : _x(0.0), _y(0.0), _z(0.0) {}
-    WcPt3D(GLdouble x, GLdouble y, GLdouble z) : _x(x), _y(y), _z(z) {}
+    WcPt3D(GLfloat x, GLfloat y, GLfloat z) : _x(x), _y(y), _z(z) {}
 
-    GLdouble getX() const { return _x; }
-    GLdouble getY() const { return _y; }
-    GLdouble getZ() const { return _z; }
+    GLfloat getX() const { return _x; }
+    GLfloat getY() const { return _y; }
+    GLfloat getZ() const { return _z; }
 
-    void setX(GLdouble x) { _x = x; }
-    void setY(GLdouble y) { _y = y; }
-    void setZ(GLdouble z) { _z = z; }
+    void setX(GLfloat x) { _x = x; }
+    void setY(GLfloat y) { _y = y; }
+    void setZ(GLfloat z) { _z = z; }
 
 protected:
-    GLdouble _x, _y, _z;
+    GLfloat _x, _y, _z;
 };
--- WcVector3D.h
+++ WcVector3D.h
@@ -18,10 +18,10 @@ class WcVector3D :
 {
 public:
     WcVector3D() : WcPt3D(0.0, 0.0, 0.0) {}
-    WcVector3D(GLdouble x, GLdouble y, GLdouble z) : WcPt3D(x, y, z) {}
+    WcVector3D(GLfloat x, GLfloat y, GLfloat z) : WcPt3D(x, y, z) {}
     void normalize()
     {
-        const GLdouble size = sqrt(_x * _x + _y * _y + _z * _z);
+        const GLfloat size = sqrt(_x * _x + _y * _y + _z * _z);
         _x = _x / size;
         _y = _y / size;
         _z = _z / size;
